60f08dadef08306753927926a1292ff9070d3e9e
Gang Zhang <gzhangx@hotmail.com>
2022-03-15 23:59:00 -0400

    'test

--- components/generic/EditTextDropdown.tsx
+++ components/generic/EditTextDropdown.tsx
@@ -1,4 +1,4 @@
-import { useState, Dispatch, SetStateAction } from 'react';
+import { useState, Dispatch, SetStateAction, useEffect } from 'react';
 import { GenericDropdown, IGenericDropdownProps } from './GenericDropdown'
 
 export interface IEditTextDropdownItem {
@@ -15,25 +15,23 @@ interface IEditTextDropdownProps extends IGenericDropdownProps {
 
 export function EditTextDropdown(props: IEditTextDropdownProps) {
     const opts = props.opts || {};
-    const [oselected, osetSelected] = useState<IEditTextDropdownItem>(null);    
-    if (!opts.setSelected) {        
-        opts.selected = oselected;
-        opts.setSelected = osetSelected;
-    }
-
-    let setTo = null;
-    if (!opts.selected) {
-        if (props.items.length) {
-            setTo = (props.items[0])
-        }
-    }
-    if (props.selected) {
-        setTo = props.selected;        
-    }
-    if (setTo) {
-        opts.setSelected(setTo);
-        props.onSelectionChanged(setTo);
-    }
+    const [selected, setSelected] = useState<IEditTextDropdownItem>(null);    
+    
+    opts.selected = selected;
+    opts.setSelected = setSelected;
+    useEffect(() => {
+        let setTo = props.selected;
+        if (!setTo) {
+            if (props.items.length) {
+                setTo = (props.items[0])
+            }
+        }    
+        if (setTo) {
+            setSelected(setTo);
+            props.onSelectionChanged(setTo);
+        }    
+    },[])
+    
    
     return <GenericDropdown {...props}        
         opts={opts}
