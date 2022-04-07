f845bffc56ab2a2ceb30f7794e5867b70a2a4b20
Gang Zhang <gzhangx@hotmail.com>
2022-04-07 14:13:46 -0400

    test

--- pages/util/dev.tsx
+++ pages/util/dev.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { googleSheetRead } from '../../components/api'
 import { ImportPage} from '../../components/page/imports/import'
 export function DevelopPage() {
-
-    const sheetId = '1UU9EYL7ZYpfHV6Jmd2CvVb6oBuQ6ekTR7AWXIlMvNCg';
     return <div className="container-fluid">
         <ImportPage/>        
-
-        <div className="row">
-        
+        <div className="row">        
         </div>
     </div>
 }
-
 
 export default DevelopPage;
