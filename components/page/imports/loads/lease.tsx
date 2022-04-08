3059db96eba28f8b80af94be1537f64538562a41
Gang Zhang <gzhangx@hotmail.com>
2022-04-08 18:33:57 -0400

    tenant

--- components/page/imports/loads/lease.tsx
+++ components/page/imports/loads/lease.tsx
@@ -1,6 +1,7 @@
 
 import { IBasicImportParams, IPaymentWithArg, IPageInfo, IItemData, IDataDetails, IPageStates } from '../types'
 import { googleSheetRead, getOwners, sqlAdd, getHouseInfo, getPaymentRecords } from '../../../api'
+import { keyBy } from 'lodash'
 
 interface ITenantInfo {
     'firstName': string;
@@ -9,11 +10,45 @@ interface ITenantInfo {
     'phone': string;
     'email': string;
 }
-export async function lease_PageLoader(pageState: IPageStates) : Promise <void> {
-    const tenants = await googleSheetRead(pageState.sheetId, 'read', `'Tenants Info'!A1:M`);
+
+async function getTenants(pageState: IPageStates) {
+    const tenantsRaw = await googleSheetRead(pageState.sheetId, 'read', `'Tenants Info'!A1:M`);
     const tenantFieldMap = ['', 'firstName', 'lastName', 'fullName', 'phone', 'email'];
-    tenants.values;
+    const tenants = tenantsRaw.values.map(r => {
+        return tenantFieldMap.reduce((acc, f, ind) => {
+            if (f) {
+                acc[f] = r[ind];
+            }
+            return acc;
+        }, {} as ITenantInfo);
+    });
+    return tenants;
 }
+
+export const LEASEINFO_ROW_STATE_NAME = 'LEASEINFO_ROW_STATE_NAME';
+interface ILeaseInfoRowState {
+    tenantFound: boolean;
+    tenantxFound: ITenantInfo[];    
+}
+
+export async function lease_PageLoader(pageState: IPageStates) : Promise <void> {
+    const tenants = await getTenants(pageState);
+    const tenantsByFullName = keyBy(tenants, t => t.fullName.toLowerCase());
+    pageState.pageDetails.rows.map(r => {        
+        const rState = {
+            tenantxFound: [],
+        } as ILeaseInfoRowState;
+        r[LEASEINFO_ROW_STATE_NAME] = {
+            val: '',
+            obj: rState,
+        };
+        [1, 2, 3, 4].forEach(ti => {
+            const tn = tenantsByFullName[(r[`tenant${ti}`].val || '').toLowerCase()]
+            rState.tenantxFound[ti] = tn;
+        });
+    });
+}
+
 export function lease_DisplayItem(state: IPageStates, field: string, itm: IItemData, all: { [key: string]: IItemData }, rowInd: number): JSX.Element | string {
     return '';
 }
