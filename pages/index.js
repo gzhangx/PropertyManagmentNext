3e8926142cd04a6f411de9cf9da26af8097e0713
Gang Zhang <gzhangx@hotmail.com>
2022-08-11 12:01:48 -0400

    fix login

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CRoute from './[route]'
 import Login from './Login'
 
@@ -11,14 +11,17 @@ export default function Home1() {
             isLoggedIn: false,
         }
     });
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const rs = RootState.useRootPageContext();
-    if (rs?.userInfo?.id) {
+    useEffect(() => {
+        setIsLoggedIn(!!rs?.userInfo?.id);
+    }, [rs?.userInfo?.id]);
+    if (isLoggedIn) {
         console.log('has login', rs.userInfo)
         //static page hack https://www.alpeaudio.com/post/hosting-a-next-js-app-with-routing-on-aws-s3/    
         return <CRoute />
     }
-    console.log('no login div test')
-    return <div>test test</div>
     //if (mainState.userInfo.isLoggedIn)
     //    return <Home state={mainState} setMainState={setMainState}></Home>
     //else 
