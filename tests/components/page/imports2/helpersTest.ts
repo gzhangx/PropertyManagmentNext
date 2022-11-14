0a70a5ad3830fe3dae89a91d5b5445668698d951
Gang Zhang <gzhangx@hotmail.com>
2022-11-14 14:47:23 -0500

    test

--- tests/components/page/imports2/helpersTest.ts
+++ tests/components/page/imports2/helpersTest.ts
@@ -17,10 +17,10 @@ describe('HelperTests', function(){
             matcherName: '',
         } as ISheetRowData;
     });
-    /*
+
     function getFakeSheetRowData(ref: string) : ISheetRowData {
         return {
-            dataType: 'Sheet' as ROWDataType,
+            dataType: 'Sheet',
             needUpdate: false,
             displayData: null,
             importSheetData: {
@@ -30,29 +30,32 @@ describe('HelperTests', function(){
             matched: null,
             matchToKey: '',
             matcherName: '',
-        } as ISheetRowData;
+        };
     }
     function getFakeDbRowData(ref: string) : IDbRowMatchData {
         return {
-            dataType: 'DB' as ROWDataType,
+            dataType: 'DB',
             dbItemData: {
                 ref,
             },
             matchedToKey: '',
-        } as IDbRowMatchData;
+        };
     }
+
     it('should matchItem ', function() {
         const sheetData: ISheetRowData[] =[
             getFakeSheetRowData('1'),
         ];
         const dbData: IDbRowMatchData[] = [getFakeDbRowData('1')];
+        /*
         matchItems(sheetData, dbData, {
             name:'fake',
             getRowKey: (data: IDbSaveData, src) => {
                 return data.ref as string;
             }
         })
-    })
 
-     */
+         */
+    });
+
 })
