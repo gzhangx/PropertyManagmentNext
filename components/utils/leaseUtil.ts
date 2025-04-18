84f7aeea1cc8eda427c5493655021e53d427e47e
Gang Zhang <gzhangx@hotmail.com>
2025-04-17 23:55:43 -0400

    day 5 as due date

--- components/utils/leaseUtil.ts
+++ components/utils/leaseUtil.ts
@@ -210,7 +210,7 @@ export async function gatherLeaseInfomation(house: HouseWithLease, date?: Date) 
         return 'Lease not found';
     }
     const payments = await finder.loadLeasePayments(lease);
-    const leaseBalance = finder.calculateLeaseBalances(lease, payments, 3, new Date());
+    const leaseBalance = finder.calculateLeaseBalances(lease, payments, 5, new Date());
 
     leaseBalance.monthlyInfo.reverse();
     house.lease = lease;
