cacdc3b5df920584decd688e63776413a55fef99
jinlinx <71112236+jinlinx@users.noreply.github.com>
2025-05-16 00:49:15 -0400

    add editTitle to the add/edit page

--- components/uidatahelpers/GenCrudAdd.tsx
+++ components/uidatahelpers/GenCrudAdd.tsx
@@ -13,6 +13,7 @@ import { ALLFieldNames, DataToDbSheetMapping, ICrudAddCustomObj, ItemType, ItemT
 import { usePageRelatedContext } from '../states/PageRelatedState';
 import { IHelper } from '../reportTypes';
 import GrkEditableDropdown from '../generic/GrkEditableDropdown';
+import { TextAlignment } from 'pdf-lib';
 
 
 
@@ -29,7 +30,7 @@ export interface IGenGrudAddProps extends IGenGrudProps {
         
     operation: 'Add' | 'Update';
     show: boolean;
-    desc?: string;    
+  
     sheetMapping?: DataToDbSheetMapping;
 
     crudAddCustomObjMap: ICrudAddCustomObj<unknown>;
@@ -45,7 +46,7 @@ export const GenCrudAdd = (props: IGenGrudAddProps) => {
         onError,
         show,
         table,
-        desc,
+
         //fkDefs,
         operation,
 
@@ -211,12 +212,12 @@ export const GenCrudAdd = (props: IGenGrudAddProps) => {
         <div className="modal-dialog" role="document" style={{ maxWidth: '60%' }}>
         <div className="modal-content">
             <div className="modal-header">
-                    <h5 className="modal-title">{desc}</h5>
+                    <h5 className="modal-title centered-container headerFont" style={{textAlign:'center'}}>{props.editTitle}</h5>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true" onClick={internalCancel}>&times;</span>
                 </button>
             </div>                
-                <table>
+                <table className='centered-table'>
                 {
                     columnInfo.map((c, cind) => {
                         if (isCreateAddNewItem) { //operation === 'Add') { //  !editItem
