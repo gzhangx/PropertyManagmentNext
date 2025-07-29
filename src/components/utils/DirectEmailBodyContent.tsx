524a7b3bc82ba1cb5442e830b16c067aafdad764
jinlinx <71112236+jinlinx@users.noreply.github.com>
2025-07-28 23:27:15 -0400

    1. only has past due rent will show the balance details. 2.  consolidate the email format css.

--- src/components/utils/DirectEmailBodyContent.tsx
+++ src/components/utils/DirectEmailBodyContent.tsx
@@ -21,20 +21,36 @@ export function DirectEmailBodyContent(props: RenderProps) {
     if (!props.house.lease) return;
     const lastPaymentAmount = formatAccounting(leaseBalanceDueInfo.lastPaymentAmount);
     const lastPaymentDate = standardFormatDate(leaseBalanceDueInfo.lastPaymentDate);
-    return <>
-        <div>Dear {props.tenants.map(t => t.firstName).join(', ')}, <br />
+    const emailtableCss:React.CSSProperties = {
+         borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',
+    };
+    const emailTableWidth:React.CSSProperties= 
+    {
+        ...emailtableCss, width:'60%'
+    };
 
-            Your Rent Payment of {lastPaymentAmount} was received on {lastPaymentDate}.<br></br>
-            Here is your balance in details,<br></br>
-            <table style={{ width: '60%',  borderStyle: 'solid', borderWidth:'1px',    marginLeft: '15px', marginTop: '15px', marginRight:'15px', marginBottom:'15px',}}>
-                < tr style={{ borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px',}}>
-                    <td style={{ borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px', }}>Date </td>
-                    <td style={{borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px', }}>Transaction</td >
-                    <td style={{borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px',  paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}>Amount </td>
+     const emailTableTitle:React.CSSProperties= 
+    {
+        ...emailtableCss, 
+    };
+      return <>
+        <div>Dear {props.tenants.map(t => t.firstName).join(' and  ')}, <br /><br></br>
+
+            Your Rent Payment of {lastPaymentAmount} was received on {lastPaymentDate}.<br></br><br></br>
+
+            {
+
+               (leaseBalanceDueInfo.totalBalance > 0) && <>
+            Here is your balance in details, <br></br><br></br>
+            <table style={emailTableWidth}>
+                < tr style={emailtableCss}>
+                    <td style={emailtableCss}>Date </td>
+                    <td style={emailtableCss}>Transaction</td >
+                    <td style={emailtableCss}>Amount </td>
                 </tr >
-                <tr  style={{ borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px',}}>
-                    <td colSpan={2} style={{ borderCollapse: 'collapse', textAlign:'right',  borderStyle: 'solid', borderWidth: '1px',  paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}> Previous Balance: </td>
-                    <td style={{borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px',  paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}>{ formatAccounting(leaseBalanceDueInfo.balanceForwarded)}</td >
+                <tr  style={emailtableCss}>
+                    <td colSpan={2} style={{textAlign:'right',}}> Previous Balance: </td>
+                    <td style={emailtableCss}> {formatAccounting(leaseBalanceDueInfo.balanceForwarded)}</td >
                 </tr >
                 {
                     leaseBalanceDueInfo.lastNPaymentAndDue.map((info, key) => {
@@ -45,16 +61,16 @@ export function DirectEmailBodyContent(props: RenderProps) {
                         } else {
                             type = 'Rent Due';
                         }
-                        return <tr key={key}  style={{ borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px',}}>
-                                    <td style={{borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}>{standardFormatDate(info.date)} </td>
-                                    <td style={{borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '0.5px',  paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}>{type}</td> 
-                                    <td style={{borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}>{ formatAccounting(amt)} </td>
+                        return <tr key={key}  style={emailtableCss}>
+                                    <td style={emailtableCss}>{standardFormatDate(info.date)} </td>
+                                    <td style={emailtableCss}>{type}</td> 
+                                    <td style={emailtableCss}>{ formatAccounting(amt)} </td>
                                 </tr >
                     })
                 }
-                <tr  style={{ borderCollapse: 'collapse', borderStyle: 'solid', borderWidth: '1px',}}>
-                    <td colSpan={2} style={{ borderCollapse: 'collapse', fontWeight:'bold', textAlign:'right', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px', }}>Current Balance:</td>
-                    <td style={{borderCollapse: 'collapse', fontWeight:'bold', borderStyle: 'solid', borderWidth: '1px', paddingLeft:'15px', paddingRight:'15px', paddingTop:'15px', paddingBottom:'15px',}}>{ formatAccounting(leaseBalanceDueInfo.totalBalance)}</td >
+                <tr  style={emailtableCss}>
+                    <td colSpan={2} style={emailtableCss}>Current Balance:</td>
+                    <td style={emailtableCss}>{formatAccounting(leaseBalanceDueInfo.totalBalance)}</td >
                 </tr>
                 
             </table>
@@ -65,17 +81,18 @@ export function DirectEmailBodyContent(props: RenderProps) {
 props.house.lease.petDeposit && <>Pet Depsoist Received: {formatAccounting(props.house.lease.petDeposit)} <br></br>
 </>
             }
-            
+           
 
             <br></br>Please let me know if you have any questions or concerns.<br></br>
+             </>}
             Thank you,<br></br>
              {props.senderName}<br></br><br></br>
             **************************************************************************************************************************<br></br>
             This is an automatically generated email. Please do not reply, as this email address is not monitored.<br></br>
             If you have any questions or concerns, feel free to contact us by email at {props.contactEmail} or text @ { props.contactPhone}.<br/>
             **************************************************************************************************************************
-        </div>
-    </>
+        
+   </div> </>
 }
 
 
