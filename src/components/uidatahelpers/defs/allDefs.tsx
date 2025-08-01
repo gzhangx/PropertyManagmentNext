9c4605ff67ad491dfe3aa6478314d5365399b4ed
Gang Zhang <gzhangx@hotmail.com>
2025-07-31 22:22:45 -0400

    split out leasinfo

--- src/components/uidatahelpers/defs/allDefs.tsx
+++ src/components/uidatahelpers/defs/allDefs.tsx
@@ -1,5 +1,6 @@
 import { TableNames } from "../../types";
 import { ALLFieldNames, ITableAndSheetMappingInfo } from "../datahelperTypes";
+import { leaseInfoDef } from "./leaseInfoDef";
 
 export const workerInfoDef: ITableAndSheetMappingInfo<unknown> = {
     table: 'workerInfo',
@@ -156,60 +157,6 @@ editTitle:'Add/Edit Maintenance Record',
         return customHeaderFilterFuncWithHouseIDLookup(mainCtx, pageState, colInfo, 'maintenanceRecords');
         },
 
-}
-
-
-export const leaseInfoDef: ITableAndSheetMappingInfo<unknown> = {
-    table: 'leaseInfo',
-    sheetMapping: {
-        sheetName: 'Lease Info',
-        range: 'A1:Q',
-        mapping: [
-            'leaseID',
-            'houseID',
-            'startDate',
-            'endDate',
-            'monthlyRent',
-            'deposit',
-            'petDeposit',
-            'otherDeposit',
-            'comment',
-            'reasonOfTermination',
-            'terminationDate',
-            'terminationComments',
-            'tenant1',
-            'tenant2',
-            'tenant3',
-            'tenant4',
-            'tenant5', //not here, added to force mapping
-            '',
-            'rentDueDay',
-        ],
-    },
-    otherFieldOverrideFields: {
-        rentDueDay: 5,
-    },
-    editTitle: 'Add/Edit Lease Record',
-    displayFields: 
-        [
-        { field: 'houseID', desc: 'House' },
-        { field: 'startDate', desc: 'Start Date', displayType: 'date' },
-        { field: 'endDate', desc: 'End Date', displayType: 'date' },
-        { field: 'monthlyRent', desc: 'Rent', displayType: 'currency' },
-        { field: 'tenant1', desc: 'Tenant1' },
-        { field: 'tenant2', desc: 'Tenant2' },
-        { field: 'tenant3', desc: 'Tenant3' },        
-    ],
-    customAddNewDefaults: async (mainCtx, columnInfo, editItem) => {
-        for (const c of columnInfo) {
-            if (c.field === 'terminationDate') {
-                editItem.data[c.field] = '';
-            } else if (c.field === 'endDate') {
-                editItem.data[c.field] = '';
-            }
-            console.log('debugremove lease', c.field, editItem.data[c.field as ALLFieldNames])
-        }
-    },
 }
 
 
